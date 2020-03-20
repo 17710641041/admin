072c14a4ea16ce5e4b980132ab4bfe7ddc46fed2
songguojun <gj_yxcom@163.com>
2020-03-20 22:59:53 +0800

    提交

---
+++ main.py
@@ -0,0 +1,40 @@
+"""
+# python3
+# -*- coding:utf-8 -*-
+# Author: 宋国君
+# Email: 673903363.com
+# Version: 商城项目入口文件
+"""
+import platform
+import tornado.ioloop
+import tornado.web
+import os
+import sys
+from tornado.options import define, options
+from common.url_router import include, url_wrapper
+
+# windows 系统下 tornado 使用 使用 SelectorEventLoop
+if platform.system() == "Windows":
+    import asyncio
+    asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
+
+
+class Application(tornado.web.Application):
+    def __init__(self):
+        handlers = url_wrapper([
+            (r"/users/", include('views.users.users_urls'))
+        ])
+        # 定义 Tornado 服务器的配置项，如 static/templates 目录位置、debug 级别等
+        settings = dict(
+            debug=True,
+            static_path=os.path.join(os.path.dirname(__file__), "static"),
+            template_path=os.path.join(os.path.dirname(__file__), "templates")
+        )
+        tornado.web.Application.__init__(self, handlers, **settings)
+
+
+if __name__ == '__main__':
+    print("Tornado server is ready for service\r")
+    tornado.options.parse_command_line()
+    Application().listen(8000, xheaders=True)
+    tornado.ioloop.IOLoop.instance().start()
